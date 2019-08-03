2eca95fb9ab20bbdb598a5ae51af5887baa419ba
Sebastian Carbonero <isebasus@gmail.com>
2019-08-03 11:50:15 -0700

    Update btn.js

--- src/btn.js
+++ src/btn.js
@@ -1,1 +1,12 @@
+import React, { Component } from 'react';
+import GradientButton from 'react-linear-gradient-button';
+
+export default class Btn extends React.Component {
+
+    render() {
+        return <GradientButton>BUTTON</GradientButton>;
+      }
+   
+}
+
 

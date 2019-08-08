8fd32e6e3bf66a0b63c4a61d91672dba4e0eaa7e
Sebastian Carbonero <isebasus@gmail.com>
2019-08-08 13:09:31 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, {Component} from 'react';
-import StyledButton from './btn';
+import StyledButton from './components/btn.jsx';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="body">
-        <div className="container">
-          <p>
-            <span className="first">Hello there.</span>
-            <span className="second"> I'm a human named</span>
-            <span className="third"> Sebastian.</span>
-            <span className="fourth"> Nice to meet you.</span>
-          </p>
-          <StyledButton></StyledButton>
-        </div>
-      </header>
-    </div>
+    <Router>
+       <div className="App">
+          <Route exact path="/" component={Home}/>
+          <Route path="/portfolio" component={Portfolio}/>
+      </div>
+    </Router>
   );
+}
+
+function Home(){
+  return (
+    <header className="body">
+      <div className="container">
+        <p>
+          <span className="first">Hello there.</span>
+          <span className="second"> I'm a human named</span>
+          <span className="third"> Sebastian.</span>
+          <span className="fourth"> Nice to meet you.</span>
+        </p>
+          <StyledButton></StyledButton>
+      </div>
+    </header>
+  )
+}
+
+function Portfolio(){
+  return(
+    <p>hello</p>
+  )
 }
 
 export default App;

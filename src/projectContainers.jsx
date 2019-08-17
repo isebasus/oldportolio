a26100301c32e1fbdf046a0ca98c185a2ad30271
Sebastian Carbonero <isebasus@gmail.com>
2019-08-17 15:28:23 -0700

    Create projectContainers.jsx

---
+++ src/projectContainers.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import injectSheet from 'react-jss';
+
+const styles = {
+
+}
+
+const container = ({ }) => {
+    return(
+        <div></div>
+    )
+};
+
+const projectContainer = injectSheet(styles)(container)
+
+export default projectContainer;

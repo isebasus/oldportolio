4d575cb66267deffc367a8c68dbaaed4385bb322
Sebastian Carbonero <isebasus@gmail.com>
2019-08-25 15:45:32 -0700

    Update projectContainers.jsx

--- src/projectContainers.jsx
+++ src/projectContainers.jsx
@@ -3,26 +3,62 @@ import injectSheet from 'react-jss';
 
 const styles = {
     box: {
-        marginTop: '14.5%',
+        marginTop: '16%',
+        borderRadius: '15px',
     },
     caption:{
-        marginTop: '-75px',
+        position: 'relative'
     },
     header: {
-        marginLeft: 0,
+        position: 'relative',
+        lineHeight: '90%',
+    },
+    button: {
+        background: 'linear-gradient(to right, #0cebeb, #20e3b2, #29ffc6)',
+        color: '#F1F1F1',
+        borderRadius: '5px',
+        border: '0px', 
+        fontWeight: 700,
+        userSelect: 'none',
+        textDecoration: 'none',
+        animation: 'show 1s infinite',
+        cursor: 'pointer',
+        textTransform: 'uppercase',
+        padding: '1vw 1.5vw',
+        fontSize: '.84vw',
+        letterSpacing: '0.2vw',
+        display: 'incline-block',
+        verticalAlign: 'middle'
+    },
+    animation: {
+
+        textAlign: 'left',
+        position: 'relative',
+        marginLeft: '500px'
+    },
+    positioning: {
+
     }
+
 };
 
 const container = ({ header, caption}) => {
     return(
-        <div style = { styles.box }>
-            <div style = { styles.header }>
-                <p className="header">{ header }</p>
+            <div style = { styles.box }>
+                <div style = { styles.header }>
+                    <h1 className="header">{ header }</h1>
+                </div>
+                <div style = { styles.positioning }>
+                    <div style = { styles.animation }>
+                        <button style = { styles.button }type="button">
+                        View Project
+                        </button>
+                    </div>
+                    <p className="caption" style = {styles.caption}>
+                        {caption}
+                    </p>
+                </div>
             </div>
-            <div className="caption" style = {styles.caption}>
-                {caption}
-            </div>
-        </div>
     )
 };
 

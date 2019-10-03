e645863bd30782d332bb2989041da52b06e8e966
Sebastian Carbonero <isebasus@gmail.com>
2019-10-02 21:05:07 -0700

    Update projectContainers.jsx

--- src/projectContainers.jsx
+++ src/projectContainers.jsx
@@ -5,56 +5,51 @@ import PropTypes from "prop-types";
 import projectDescription from './description';
 
 
-
-const styles = {
-    box: {
-        borderRadius: '15px',
-        textDecoration: 'none',
-    },
-    caption:{
-        position: 'relative',
-        textDecoration: 'none',
-        textRendering: 'optimizeLegibility',
-    },
-    header: {
-        position: 'relative',
-        lineHeight: '90%',
-        textDecoration: 'none',
-        textRendering: 'auto',
-    },
-    button: {
-        background: 'linear-gradient(to right, #0cebeb, #20e3b2, #29ffc6)',
-        color: '#F1F1F1',
-        borderRadius: '5px',
-        border: '0px', 
-        fontWeight: 700,
-        userSelect: 'none',
-        textDecoration: 'none',
-        animation: 'show 1s infinite',
-        cursor: 'pointer',
-        textTransform: 'uppercase',
-        letterSpacing: '0.2vw',
-        verticalAlign: 'middle',
-    },
-    animation: {
-        position: 'relative',
-        textAlign: 'left',
-        marginLeft: '42%',
-    },
-};
-
-const hello = () =>{
-    alert("hello");
-}
-
-const container = ({ header, caption}) => {
+    const styles = {
+        box: {
+            borderRadius: '15px',
+            textDecoration: 'none',
+        },
+        caption:{
+            position: 'relative',
+            textDecoration: 'none',
+            textRendering: 'optimizeLegibility',
+        },
+        header: {
+            position: 'relative',
+            lineHeight: '90%',
+            textDecoration: 'none',
+            textRendering: 'auto',
+        },
+        button: {
+            background: 'linear-gradient(to right, #0cebeb, #20e3b2, #29ffc6)',
+            color: '#F1F1F1',
+            borderRadius: '5px',
+            border: '0px', 
+            fontWeight: 700,
+            userSelect: 'none',
+            textDecoration: 'none',
+            animation: 'show 1s infinite',
+            cursor: 'pointer',
+            textTransform: 'uppercase',
+            letterSpacing: '0.2vw',
+            verticalAlign: 'middle',
+        },
+        animation: {
+            position: 'relative',
+            textAlign: 'left',
+            marginLeft: '43%',
+        },
+    };
+        
+const container = ({ header, caption, onClick}) => {
     return(
             <div className="projectcontainer" style = { styles.box }>
                 <div style = { styles.header }>
                     <h1 className="header">{ header }</h1>
                 </div>
                 <div className="buttonlocation" style = { styles.animation }>
-                    <button className="projectbutton" style = { styles.button }type="button" onClick={hello}>
+                    <button className="projectbutton" style = { styles.button } type="button" onClick={onClick}>
                         View Project
                     </button>
                 </div>
@@ -62,13 +57,13 @@ const container = ({ header, caption}) => {
                     {caption}
                 </p>
                 <div style = { styles.animation }>
-                    <button className="responsivebutton" style = { styles.button }type="button">
+                    <button className="responsivebutton" style = { styles.button }type="button" onClick={onClick}>
                         View Project
                     </button>
                 </div>
             </div>
-    )
-};
+        )
+    };
 
 const Container = injectSheet(styles)(container);
 
